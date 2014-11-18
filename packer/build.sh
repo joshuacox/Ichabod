92c791baa2a2880aa988cec1fa6406a776138729
Josh Cox <josh@webhosting.coop>
2014-11-18 16:15:05 -0600

    RC 1

--- packer/build.sh
+++ packer/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-rm -r vendor
-rm Berksfile.lock
-berks vendor vendor/cookbooks
-sed -i 's/d-i\ mirror\/http\/proxy\ string/d-i\ mirror\/http\/proxy\ string\ http:\/\/65.67.51.187:3142/' http/debian-7/preseed.cfg
-#/usr/bin/time -v packer build -debug --only=virtualbox-iso pdd-debian-7.7-i386.json
-#/usr/bin/time -v packer build --only=virtualbox-iso pdd-debian-7.7-i386.json
-#/usr/bin/time -v packer build -debug --only=qemu pdd-debian-7.7-i386.json
-/usr/bin/time -v packer build --only=qemu pdd-debian-7.7-i386.json
-sed -i 's/d-i\ mirror\/http\/proxy\ string\ http:\/\/65.67.51.187:3142/d-i\ mirror\/http\/proxy\ string/' http/debian-7/preseed.cfg
+#rm -r vendor
+#rm Berksfile.lock
+#berks vendor vendor/cookbooks
+#sed -i 's/d-i\ mirror\/http\/proxy\ string/d-i\ mirror\/http\/proxy\ string\ http:\/\/65.67.51.187:3142/' http/debian-7/preseed.cfg
+#/usr/bin/time -v packer build -debug --only=virtualbox-iso Ichabod-debian-7.7-i386.json
+#/usr/bin/time -v packer build --only=virtualbox-iso Ichabod-debian-7.7-i386.json
+#/usr/bin/time -v packer build -debug --only=qemu Ichabod-debian-7.7-i386.json
+#/usr/bin/time -v packer build --only=qemu Ichabod-debian-7.7-i386.json
+#sed -i 's/d-i\ mirror\/http\/proxy\ string\ http:\/\/65.67.51.187:3142/d-i\ mirror\/http\/proxy\ string/' http/debian-7/preseed.cfg
 # beeper
 #aplay /usr/share/sounds/alsa/Front_Center.wav
 # ogg123 /usr/share/sounds/KDE-Im-Irc-Event.ogg
+make ichabod
